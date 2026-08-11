a690f8c191efc9c8717a9103c3e3f9d75f7ecd4d
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-08-11 15:50:36 -0400

    reworked list entirely, less complicated

--- data-structures/list/list.c
+++ data-structures/list/list.c
@@ -1,47 +1,131 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 
 #include "list.h"
 
+struct list* create_list() {
 
-//https://nachtimwald.com/2020/04/09/generic-list-in-c/
+    struct list* new_list = malloc(sizeof(list_t));
+    if (new_list == NULL) {
+        perror("Error initilizing new list");
+        return NULL;
+    }
 
-static const size_t list_block_size = 32;
-struct list {
-    void **elements;
-    size_t alloced;
-    size_t len;
-    list_cbs_t cbs;
-    list_flags_t flags;
-    bool inbulk;
-};
+    new_list -> head = NULL;
+    new_list -> tail = NULL;
+    new_list -> size = 0;
 
-static int list_passthrough_eq(const void *a, const void *b) {
-    void *sa = *(void **)a;
-    void *sb = *(void **)b;
-
-    return sa - sb;
+    return new_list;
 }
 
-list_t *init_list(const list_cbs_t *cbs, list_flags_t flags) {
-    list_t *list;
+int clear_list(struct list* list) {
 
-    list = malloc(sizeof(*list));
-    list->elements = malloc(sizeof(*list->elements)*list_block_size);
-    list->alloced = list_block_size;
-    list->len = 0;
-
-    memset(&(list->cbs), 0, sizeof(list->cbs));
-    if(cbs != NULL) {
-        list->cbs.leq = cbs->leq;
-        list->cbs.lcopy = cbs->lcopy;
-        list->cbs.lfree = cbs->lfree;
-    }
-    if(list->cbs.leq == NULL) {
-        list->cbs.leq = list_passthrough_eq;
+    if(list == NULL) {
+        perror("List to clear is NULL!");
+        return FAILURE;
     }
 
-    list->flags = flags;
+    struct node* current = list->head;
+    while(current != NULL) {
+        struct node* to_free = current;
+        current = current->next;
+        free(to_free);
+    }
 
-    return list;
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
+
+int destroy_list(struct list* list) {
+    if (list == NULL) {
+        perror("List to destory is NULL!");
+        return FAILURE;
+    }
+
+    clear_list(list);
+    free(list);
+
+    return SUCCESS;
+}
+
+int size_list(struct list* list) {
+    
+    if (list == NULL) {
+        perror("List is NULL for size function!");
+        return FAILURE;
+    }
+    
+    return list->size;
+}
+
+void* head_list(struct list* list) {
+    
+    if (list == NULL) {
+        perror("List is NULL for front function!");
+        return NULL;
+    }
+
+    if (size_list(list) == 0) {
+        return NULL;
+    }
+
+    return list->head->data;
+}
+
+void* tail_list(struct list* list) {
+    
+    if (list == NULL) {
+        perror("List is NULL for front function!");
+        return NULL;
+    }
+
+    if (size_list(list) == 0) {
+        return NULL;
+    }
+
+    return list->tail->data;
+}
+
+int set_list(struct list* list, int index, void* data) {
+
+     if (list == NULL) {
+        perror("List is NULL for front function!");
+        return FAILURE;
+    }
+
+    if (index < 0 || index > size_list(list)) {
+        perror("set_list invalid index!");
+        return FAILURE;
+    }
+
+    struct node * current = head_list(list);
+    for (int i = 0; i < index; i++) {
+        current = current->next;
+    }
+    
+    current->data = data;
+
+    return SUCCESS;
+}
+
+void* get_list(struct list* list, int index) {
+
+     if (list == NULL) {
+        perror("List is NULL for front function!");
+        return NULL;
+    }
+
+     if (index < 0 || index > size_list(list)) {
+        perror("set_list invalid index!");
+        return FAILURE;
+    }
+
+    struct node * current = head_list(list);
+    for (int i = 0; i < index; i++) {
+        current = current->next;
+    }
+    
+    return current->data;
 }
--- data-structures/list/list.h
+++ data-structures/list/list.h
@@ -3,32 +3,37 @@
 
 #pragma once
 #include <stdlib.h>
-#include <string.h>
 
-struct List;
-typedef struct list list_t;
-
-/*
-* callback functions
-*/
-typedef int (*list_eq)(const void *a, const void *b);
-typedef void *(*list_copy_cb)(void *);
-typedef void (*list_free_cb)(void *);
-
-typedef struct {
-    list_eq leq;
-    list_copy_cb lcopy;
-    list_free_cb lfree;
-} list_cbs_t;
-
-/**
-* flags
- */
 typedef enum {
-    LIST_NONE = 0,
-    LIST_SORT = 1 << 0
-} list_flags_t;
+    SUCCESS = 0,
+    FAILURE = -1,
+} return_t;
 
-extern list_t *init_list(const list_cbs_t *cbs, list_flags_t flags);
-extern void list_destroy(list_t *l);
+typedef struct node {
+    
+    void* data;
+    struct node* next;
+    struct node* prev;
+
+} list_node_t;
+
+typedef struct list {
+    
+    int size;
+    struct node* head;
+    struct node* tail;
+
+} list_t;
+
+
+list* create_list();
+int destroy_list(list*);
+int clear_list(list*);
+int size_list(list*);
+
+void* head_list(list*);
+void* tail_list(list*);
+int set_list(list*, int index, void* data);
+void* get_list(list*, int index);
+
 #endif
