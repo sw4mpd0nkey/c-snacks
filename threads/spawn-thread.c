5d01642328cd6d0afa9dcc0a24df436520d5e5b2
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-08-12 15:54:11 -0400

    most basic example, doesn't have logic to wait for threads to join

---
+++ threads/spawn-thread.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+
+// when compiling remember to pass the -pthread flag into gcc
+// e.g: gcc -o spawn-thread spawn-thread.c -pthread
+
+#define THREAD_COUNT 10
+
+// Thread function to execute.
+void *thread_function(void *arg) {
+    printf("Hello from the thread!\n");
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[THREAD_COUNT];
+    
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        /* Create the thread;
+         * int pthread_create(pthread_t *restrict thread,
+                          const pthread_attr_t *_Nullable restrict attr,
+                          typeof(void *(void *_Nullable)) *start_routine,
+                          void *_Nullable restrict arg);
+            NOTE - if you need to pass many args wrap tnem in a strut
+        */
+        if (pthread_create(&threads[i], NULL, thread_function, NULL) != 0) {
+            perror("Failed to create thread");
+            return -1;
+        }   
+    }
+
+    return 0;
+}
