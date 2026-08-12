ef791247782d6e356e636f2691348a2002d6888c
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-08-12 15:36:36 -0400

    added generic obj for reference

---
+++ data-structures/generic-obj/generic-obj.c
@@ -0,0 +1,207 @@
+#include "generic-obj.h"
+#include <stdlib.h>
+#include <string.h>
+
+generic_object_t *generic_add(generic_object_t *a, generic_object_t *b) {
+  if (a == NULL || b == NULL) {
+    return NULL;
+  }
+
+  switch (a->kind) {
+    case INTEGER:
+      if (b->kind == INTEGER) {
+        return new_generic_integer(a->data.v_int + b->data.v_int);
+      }
+      if (b->kind == FLOAT) {
+        return new_generic_float( ((float)a->data.v_int) + b->data.v_float);
+      }
+      return NULL;
+    case FLOAT:
+      if (b->kind == INTEGER) {
+        return new_generic_integer(((int)a->data.v_float) + b->data.v_int);
+      }
+      if (b->kind == FLOAT) {
+        return new_generic_float(a->data.v_float + b->data.v_float);
+      }
+      return NULL;
+    case STRING:
+      if (b != NULL && b->kind != STRING) {
+        return NULL;
+      } else {
+        int new_length = strlen(b->data.v_string) + strlen(b->data.v_string) +2;
+        char *new_str = calloc(new_length, sizeof(char));
+        strcat(new_str,a->data.v_string);
+        strcat(new_str, b->data.v_string);
+        generic_object_t *to_return = new_generic_string(new_str);
+        free(new_str);
+        return to_return;
+      }
+      
+      return NULL;
+    case VECTOR3:
+      if (b != NULL && b->kind != VECTOR3) {
+        return NULL;
+      } else {
+        return new_generic_vector3(
+          generic_add(a->data.v_vector3.x,b->data.v_vector3.x),
+          generic_add(a->data.v_vector3.y,b->data.v_vector3.y),
+          generic_add(a->data.v_vector3.z,b->data.v_vector3.z)
+        );
+      }
+      
+      return NULL;
+    case ARRAY:
+      if (b != NULL && b->kind != ARRAY) {
+        return NULL;
+      } else {
+        size_t new_size = a->data.v_array.size + b->data.v_array.size;
+        generic_object_t *new_array = new_generic_array(new_size);
+        int index = 0;
+        
+        for (int i = 0; i < a->data.v_array.size; i++) {
+          generic_array_set(new_array, index, generic_array_get(a,i));
+          index++;
+        }
+        for (int i = 0; i < b->data.v_array.size; i++) {
+          generic_array_set(new_array, index, generic_array_get(b,i));
+          index++;
+        }
+
+        return new_array;
+      }
+      
+      return NULL;
+    default:
+      return NULL;
+  }
+}
+
+int generic_length(generic_object_t *obj) {
+  if (obj == NULL) {
+    return -1;
+  }
+
+  switch (obj->kind) {
+  case INTEGER:
+    return 1;
+  case FLOAT:
+    return 1;
+  case STRING:
+    return strlen(obj->data.v_string);
+  case VECTOR3:
+    return 3;
+  case ARRAY:
+    return obj->data.v_array.size;
+  default:
+    return -1;
+  }
+}
+
+generic_object_t *new_generic_array(size_t size) {
+  generic_object_t *obj = malloc(sizeof(generic_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  generic_object_t **elements = calloc(size, sizeof(generic_object_t *));
+  if (elements == NULL) {
+    free(obj);
+    return NULL;
+  }
+
+  obj->kind = ARRAY;
+  obj->data.v_array = (generic_array_t){.size = size, .elements = elements};
+  return obj;
+}
+
+bool generic_array_set(generic_object_t *array, size_t index, generic_object_t *value) {
+  if (array == NULL || value == NULL) {
+    return false;
+  }
+
+  if (array->kind != ARRAY) {
+    return false;
+  }
+
+  if (index >= array->data.v_array.size) {
+    return false;
+  }
+
+  array->data.v_array.elements[index] = value;
+  return true;
+}
+
+generic_object_t *generic_array_get(generic_object_t *array, size_t index) {
+  if (array == NULL) {
+    return NULL;
+  }
+
+  if (array->kind != ARRAY) {
+    return NULL;
+  }
+
+  if (index >= array->data.v_array.size) {
+    return NULL;
+  }
+
+  return array->data.v_array.elements[index];
+}
+
+generic_object_t *new_generic_vector3(generic_object_t *x, generic_object_t *y,
+                                generic_object_t *z) {
+  if (x == NULL || y == NULL || z == NULL) {
+    return NULL;
+  }
+
+  generic_object_t *obj = malloc(sizeof(generic_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = VECTOR3;
+  obj->data.v_vector3 = (generic_vector_t){.x = x, .y = y, .z = z};
+
+  return obj;
+}
+
+generic_object_t *new_generic_integer(int value) {
+  generic_object_t *obj = malloc(sizeof(generic_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = INTEGER;
+  obj->data.v_int = value;
+  return obj;
+}
+
+generic_object_t *new_generic_float(float value) {
+  generic_object_t *obj = malloc(sizeof(generic_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  obj->kind = FLOAT;
+  obj->data.v_float = value;
+  return obj;
+}
+
+generic_object_t *new_generic_string(char *value) {
+  generic_object_t *obj = malloc(sizeof(generic_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+
+  int len = strlen(value);
+  char *dst = malloc(len + 1);
+  if (dst == NULL) {
+    free(obj);
+    return NULL;
+  }
+
+  strcpy(dst, value);
+
+  obj->kind = STRING;
+  obj->data.v_string = dst;
+  return obj;
+}
---
+++ data-structures/generic-obj/readme.md
@@ -0,0 +1,3 @@
+# Generic Object
+
+Simple example of an object with multiple types, can be used for a variety of data types. Mainly used for reference in creating other data structures examples.
