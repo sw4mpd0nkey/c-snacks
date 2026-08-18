cd82f0885a16e9c3d371987c62ff152a17c84f7e
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-08-18 15:21:29 -0400

    updated to pass args to thread

--- threads/spawn-thread.c
+++ threads/spawn-thread.c
@@ -6,16 +6,26 @@
 
 #define THREAD_COUNT 10
 
+typedef struct {
+    int arg1;
+    short arg2;
+} thread_arg_t;
+
 // Thread function to execute.
-void *thread_function(void *arg) {
-    printf("Hello from the thread!\n");
+void *thread_function(void *vargs) {
+    thread_arg_t *args = (thread_arg_t *)vargs;
+    printf("Hello from the thread and %d!\n", args->arg1);
     return NULL;
 }
 
 int main() {
     pthread_t threads[THREAD_COUNT];
+    thread_arg_t myargs;
     
     for (int i = 0; i < THREAD_COUNT; i++) {
+
+        myargs.arg1 = i;
+
         /* Create the thread;
          * int pthread_create(pthread_t *restrict thread,
                           const pthread_attr_t *_Nullable restrict attr,
@@ -23,16 +33,16 @@ int main() {
                           void *_Nullable restrict arg);
             NOTE - if you need to pass many args wrap tnem in a strut
         */
-        if (pthread_create(&threads[i], NULL, thread_function, NULL) != 0) {
+        if (pthread_create(&threads[i], NULL, thread_function, (void *)&myargs) != 0) {
             perror("Failed to create thread");
             return -1;
         }  
-       
-        // pthread_join blocks, pthread_detach doesm't
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            pthread_join(&threads[i], NULL);   
-        }
     }
-
+    
+    // pthread_join blocks, pthread_detach doesm't
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_join(threads[i], NULL);   
+    }
+    
     return 0;
 }
